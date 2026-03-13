2a216c6cf23d8d545eb62c31b5429129c26a0906
Rinku Diwakar <prateek.diwakarr@gmail.com>
2026-03-13 17:21:04 +0530

    did some modification like imported datetime and also fix the app.py and index.html file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app=Flask(__name__)
 
@@ -8,7 +9,8 @@ def home():
     if request.method=="POST":
         name=request.form.get("name")
         message= f"Hello {name}, welcome to the Kubernetes test app!"
-    return render_template("index.html",message=message)
+    return render_template("index.html",message=message,
+                           current_time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
   
 if __name__=="__main__":
-    app.run(host="0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=5000,debug=True)
